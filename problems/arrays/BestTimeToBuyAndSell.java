0557c49132c5c4c123c211bf8c32e854d7778e40
Vishal Yadav <vishalyadav1746@gmail.com>
2021-02-16 00:23:11 +0530

    add Best Time to Buy and Sell II ( Leetcode )

---
+++ problems/arrays/BestTimeToBuyAndSell.java
@@ -0,0 +1,18 @@
+public class BestTimeToBuyAndSell {
+
+    public static void main(String[] args) {
+        int[] input = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfit(input));
+    }
+    
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+    
+}
