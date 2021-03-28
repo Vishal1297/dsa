7e9e9aefd2c04650cb18a12c13a873d72c1b18ec
Vishal Yadav <vishalyadav1746@gmail.com>
2021-03-28 19:03:33 +0530

    add: solution for key in segment java solution with complexity O(n^2)

---
+++ problems/arrays/KeyInSegment.java
@@ -0,0 +1,29 @@
+public class KeyInSegment {
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, 2, 4, 9, 3, 1, 7, 3, 11, 12, 3 };
+        int k = 3;
+        int x = 3;
+        System.out.println(isFound(arr, k, x));
+    }
+    
+    //  O(n^2)
+    public static boolean isFound(int[] arr, int key, int segementSize) {
+        boolean found = false;
+        for (int i = 0; i < arr.length; i = i + segementSize) {
+            boolean curr = false;
+            for (int j = i; j < arr.length && j < segementSize + i; j++) {
+                if (key == arr[j]) {
+                    curr = true;
+                }
+            }
+            if (curr) {
+                found = true;
+            } else {
+                found = false;
+                break;
+            }
+        }
+        return found;
+    }
+
+}
