9fa9cc52792a3f5891f3b98db8b9061a69b8421a
Vishal <vishal.yadav@fretron.com>
2021-07-24 18:34:02 +0530

    feat: add solution Insomia Cure A - codeforces

---
+++ problems/Implimentation/InsomiaCure.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class InsomiaCure {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int l = scanner.nextInt();
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int d = scanner.nextInt();
+        int damaged = 0;
+        for (int i = 1; i <= d; i++) {
+            if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+                damaged++;
+            }
+        }
+
+        System.out.println(damaged);
+        scanner.close();
+    }
+}
