7e3be40bd6349069b712890f103a55c08362ac42
Vishal Yadav <vishal.yadav@fretron.com>
2021-10-07 02:21:30 +0530

    feat(Implimentation): add solution Sereja and Dima 381 A - codeforces

---
+++ problems/Implimentation/SerejaAndDima.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SerejaAndDima {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] inputs = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            inputs[i] = scanner.nextInt();
+        }
+        int serejaPoints = 0, dimaPoints = 0;
+        int leftPtr = 0, rightPtr = n - 1;
+        boolean first = true;
+        while (leftPtr <= rightPtr) {
+            int currentMaxPoint = 0;
+            if (inputs[leftPtr] > inputs[rightPtr]) {
+                currentMaxPoint += inputs[leftPtr];
+                leftPtr++;
+            } else {
+                currentMaxPoint += inputs[rightPtr];
+                rightPtr--;
+            }
+            if (first) {
+                serejaPoints += currentMaxPoint;
+                first = false;
+            } else {
+                dimaPoints += currentMaxPoint;
+                first = true;
+            }
+        }
+        System.out.println(serejaPoints + " " + dimaPoints);
+        scanner.close();
+    }
+}
