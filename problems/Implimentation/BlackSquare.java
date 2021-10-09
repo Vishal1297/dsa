4310c707ceb96ffb005f6fd368e6147b4bb0a439
Vishal Yadav <vishal.yadav@fretron.com>
2021-10-09 21:52:26 +0530

    feat(Implimentation): add solution Black Square 431 A - codeforces

---
+++ problems/Implimentation/BlackSquare.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BlackSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int calories[] = new int[5];
+        for (int i = 1; i < 5; i++) {
+            calories[i] = scanner.nextInt();
+        }
+        String strips = scanner.next();
+        int totalCalories = 0;
+        for (int i = 0; i < strips.length(); i++) {
+            totalCalories += calories[strips.charAt(i) - '0'];
+        }
+        System.out.println(totalCalories);
+        scanner.close();
+    }
+}
