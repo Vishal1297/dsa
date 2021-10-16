71249dffc106e359f4a9609e16e53b7bac583241
Vishal Yadav <vishal.yadav@fretron.com>
2021-10-16 20:11:37 +0530

    feat(implimentation): add solution Buy A Shovel 732 A - codeforces

---
+++ problems/Implimentation/BuyAShovel.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BuyAShovel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int r = scanner.nextInt();
+        int result = k, count = 1, i = 2;
+        while (true) {
+            if (result % 10 == 0) {
+                System.out.println(count);
+                break;
+            } else if (result % 10 == r){
+                System.out.println(count);
+                break;
+            } else {
+                result = k * i;
+                count++;
+                i++;
+            }
+        }
+        scanner.close();
+    }
+}
