86303c59477cc90a2c759b081ed23868ffc66b83
Vishal Yadav <vishal.yadav@fretron.com>
2021-11-06 01:06:32 +0530

    feat(Searching): add solution Binary Search - Leetcode

---
+++ problems/searching/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int key = 5;
+        int index = binarySearch(arr, key);
+        System.out.println("Index of " + key + " is " + index);
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
