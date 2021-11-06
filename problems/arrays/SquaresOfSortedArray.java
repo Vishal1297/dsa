d3ce24086867ee2f82f15219397b1807fac87942
Vishal Yadav <vishal.yadav@fretron.com>
2021-11-06 17:24:47 +0530

    feat(Arrays): add solution Squares Of Sorted Arrays - LeetCode

---
+++ problems/arrays/SquaresOfSortedArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums = {-7, -3, 2, 3, 11};
+        int[] result = sortedSquares(nums);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+    
+    public static int[] sortedSquares(int[] nums) {
+        return Arrays.stream(nums).map(e -> e * e).sorted().toArray();
+    }
+}
