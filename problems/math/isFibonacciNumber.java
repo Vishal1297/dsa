8f6c73d9d8bf2ece0b7e50ca8118cb31d5cc555d
Vishal <vishalyadav1746@gmail.com>
2022-02-12 16:09:27 +0530

    feat: add solution - is number in fibonacci series.

---
+++ problems/math/isFibonacciNumber.java
@@ -0,0 +1,32 @@
+public class isFibonacciNumber {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (isFibonacciNumber(i)) {
+                System.out.print(i + "\t");
+            }
+        }
+        System.out.println();
+    }
+
+    // Write a method that returns true if a number is a Fibonacci number, false otherwise.
+    
+    public static boolean isFibonacciNumber(int number) {
+        if (number == 0 || number == 1) {
+            return true;
+        }
+        int first = 0;
+        int second = 1;
+        int next = first + second;
+
+        while (next <= number) {
+            if (next == number) {
+                return true;
+            }
+            first = second;
+            second = next;
+            next = first + second;
+        }
+        return false;
+    }
+}
