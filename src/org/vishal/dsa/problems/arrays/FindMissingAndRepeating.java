ec87edb97f173f697eee56468bf8539f398505da
Vishal <vishalyadav1746@gmail.com>
2022-02-15 06:58:13 +0530

    fix: solution - find missing and repeating number - GFG.

--- src/org/vishal/dsa/problems/arrays/FindMissingAndRepeating.java
+++ src/org/vishal/dsa/problems/arrays/FindMissingAndRepeating.java
@@ -1,27 +1,30 @@
 package org.vishal.dsa.problems.arrays;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class FindMissingAndRepeating {
 
     public static void main(String[] args) {
-        int N = 3;
-        int[] arr = new int[] {1, 3, 3};
+        int N = 6;
+        int[] arr = new int[]{1, 2, 3, 4, 5, 5};
         int[] result = findTwoElement(arr, N);
-        for (int num : result){
+        for (int num : result) {
             System.out.println(num);
         }
     }
 
     public static int[] findTwoElement(int arr[], int n) {
         int sum = IntStream.of(arr).distinct().sum();
-        int missingNum = (n * (n + 1))/2 - sum;
-        HashMap<int[], Long> map = Stream.of(arr).collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
-        int maxValueKey = Arrays.stream(Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey()).max().orElse(0);
-        return new int[] {maxValueKey,missingNum};
+        int missingNum = (n * (n + 1)) / 2 - sum;
+        HashMap<Integer, Long> map = Arrays.stream(arr).boxed().collect(Collectors.groupingBy(Function.identity(), HashMap::new, Collectors.counting()));
+        map.forEach((key, value) -> System.out.println("key " + key + " value " + value));
+        int maxValueKey = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+        return new int[]{maxValueKey, missingNum};
     }
 }
