3923197448c0361a753d5cf454dd0fc753525cc7
Vishal Yadav <vishalyadav1746@gmail.com>
2022-07-16 16:12:39 +0530

    add solution - Remove linked list elements.

---
+++ src/org/vishal/dsa/problems/list/RemoveLinkedListElements.java
@@ -0,0 +1,35 @@
+package org.vishal.dsa.problems.list;
+
+public class RemoveLinkedListElements {
+
+    public static ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        ListNode newHead = new ListNode(-1, head);
+        ListNode temp = newHead;
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            }else {
+                temp = temp.next;
+            }
+        }
+        return newHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
