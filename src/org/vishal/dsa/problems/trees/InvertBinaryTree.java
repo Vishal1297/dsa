3f11d5d48ba7d402b1a6824990a03be188add897
Vishal Yadav <vishalyadav1746@gmail.com>
2022-09-12 06:17:17 +0530

    trees: add solution - Invert binary tree - Leetcode.

---
+++ src/org/vishal/dsa/problems/trees/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+package org.vishal.dsa.problems.trees;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+
+}
